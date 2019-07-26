bd783637834941a9066857325c6c9fecec952b04
Isai Bismark Cortez <isai0505@gmail.com>
2019-07-26 18:25:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else array.length > 3
+    return "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]}, #{array[5]}, and #{array[6]}"
+  end
 end
+
+#def oxford_comma(arrayy)
+
+#end
