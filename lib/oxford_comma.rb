2de6f6d49c8f6e4a100c89a19f4ae4579bebdef8
Isai Bismark Cortez <isai0505@gmail.com>
2019-07-26 19:27:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,8 +7,7 @@ def oxford_comma(array)
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
     array.each do |name|
-    return "#{name,}" 
-    end
+    return "#{name,}"
   end
 end
 
