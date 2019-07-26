1d96a76f768260ea12eb65e29e11d7b10bf8b691
Isai Bismark Cortez <isai0505@gmail.com>
2019-07-26 19:29:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
@@ -6,7 +7,7 @@ def oxford_comma(array)
   elsif array.length == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-
+    binding.pry
   end
 end
 
